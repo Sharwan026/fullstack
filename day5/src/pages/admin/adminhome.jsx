d38323c2768ef959aeabee2301e837f01dfa7ffa
Sharwan026 <113581881+Sharwan026@users.noreply.github.com>
2024-01-31 15:25:00 +0530

    Add files via upload

---
+++ day5/src/pages/admin/adminhome.jsx
@@ -0,0 +1,19 @@
+// import CustomNavbar from "../../components/CustomNavbar"
+import CustomSideN from "../../components/CustomSideNav"
+// import AdminNavbar from "../../components/adminnav"
+import "../admin/adminhome.css"
+
+
+const Adminhome = () => {
+    return (
+        <div className='bod'>
+        <div className='addashboard-container'>
+            <CustomSideN/>
+        </div>
+        <div className="adminmain">
+        
+        </div>
+    </div>
+    )
+}
+export default Adminhome
