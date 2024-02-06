5e59e71be42248d8b7ad51c7b89f566783126341
Sharwan026 <113581881+Sharwan026@users.noreply.github.com>
2024-02-06 15:23:36 +0530

    Add files via upload

---
+++ day9/src/main/java/com/ecom/sharwan/SharwanApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.sharwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharwanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharwanApplication.class, args);
+	}
+
+}
